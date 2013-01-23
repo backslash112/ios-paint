783ce0712c5260cdd057ce835b433a96f599e3c1
Pavel Logvinov <paul-mail@yandex.ru>
2013-01-23 13:05:04 +0700

    Initial Commit

---
+++ MyPaint/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyPaint
+//
+//  Created by Pavel Logvinov on 23.01.13.
+//  Copyright (c) 2013 Pavel Logvinov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
