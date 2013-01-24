b91c8aaccb404b119a9fe9f09bdcc4a015ae2545
Pavel Logvinov <paul-mail@yandex.ru>
2013-01-24 20:39:23 +0700

    Prerendering implemented.

---
+++ MyPaint/PaintingRenderer.h
@@ -0,0 +1,13 @@
+//
+//  PaintingRenderer.h
+//  MyPaint
+//
+//  Created by Pavel Logvinov on 24.01.13.
+//  Copyright (c) 2013 Pavel Logvinov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PaintingRenderer : NSObject
+
+@end
---
+++ MyPaint/PaintingRenderer.m
@@ -0,0 +1,13 @@
+//
+//  PaintingRenderer.m
+//  MyPaint
+//
+//  Created by Pavel Logvinov on 24.01.13.
+//  Copyright (c) 2013 Pavel Logvinov. All rights reserved.
+//
+
+#import "PaintingRenderer.h"
+
+@implementation PaintingRenderer
+
+@end
